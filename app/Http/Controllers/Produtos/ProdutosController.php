72c9230a954992c23232c44d3d4f8a80a51f9c62
Cassiano de Souza <redhotcassiano@gmail.com>
2017-04-25 18:57:12 -0300

    Criação do Controller e Models

---
+++ app/Http/Controllers/Produtos/ProdutosController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Produtos;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ProdutosController extends Controller
+{
+  public function __construct(\App\Models\Produtos $produtos, Request $request){
+      $this->produtos = $produtos;
+      $this->request = $request;
+  }
+
+  public function create()
+  {
+      //$data = $this->request->all();
+      //return $data;
+     $result = $this->produtos->saveProduto();
+
+     if(!$result){
+       return response(['response' => 'Não foi possivel adicionar o produto!'], 400);
+     }
+
+     return response($result, 200);
+  }
+
+  public function show($id)
+  {
+      $result = $this->produtos->getProduto($id);
+
+      if(!$result){
+        return response(['response' => 'Não foi possivel encontrar o produto!'], 400);
+      }
+
+      return response($result, 200);
+  }
+
+  public function update(Request $request, $id)
+  {
+    
+    $result = $this->produtos->updateProduto($id);
+
+    if(!$result){
+      return response(['response' => 'Não foi possivel modificar o produto!'], 400);
+    }
+
+    return response($result, 200);
+
+  }
+
+  public function destroy($id)
+  {
+      //
+  }
+
+}
